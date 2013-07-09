302d986b8a849ffa2bccf2b26b8b9ce18639f496
Claudio Meinberg <claudio.meinberg@gmail.com>
2013-07-09 13:20:29 +1000

    DCP-48 - Currency Change - R$ to US$

--- app/models/backer.rb
+++ app/models/backer.rb
@@ -112,7 +112,7 @@ class Backer < ActiveRecord::Base
   end
 
   def display_value
-    number_to_currency value, unit: "R$", precision: 0, delimiter: '.'
+    number_to_currency value, unit: "US$", precision: 0, delimiter: '.'
   end
 
   def available_rewards
