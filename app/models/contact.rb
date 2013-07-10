c7588b88085f94a7eed6aaebc57683d9cd07a30e
Lyn Nagara <lyn.nagara@gmail.com>
2013-07-10 18:19:32 +1000

    contact form server side validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,7 @@
 class Contact < ActiveRecord::Base
   attr_accessible :email, :firstname, :lastname, :message, :sports
+  validates_presence_of :firstname
+  validates_presence_of :lastname
+  validates_presence_of :email
+  validates_presence_of :message
 end
