12d3fcc0e802b4d672ca209f95096ac996796538
Lyn Nagara <lyn.nagara@gmail.com>
2013-08-07 22:44:17 +1000

    add SMTP from address to contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ActionMailer::Base  
   def email_contact(contact)
     @contact = contact
-    mail(:to => "#{::Configuration[:company_name]} <#{::Configuration[:email_system]}>", :subject => "Fanfuel Contact")
+    mail(:to => "#{::Configuration[:company_name]} <#{::Configuration[:email_system]}>", :subject => "Fanfuel Contact", :from => "#{::Configuration[:company_name]} <#{::Configuration[:email_system]}>")
   end
 end
